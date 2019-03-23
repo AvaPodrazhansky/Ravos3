9bce64828ad661ae91d55806212b504dff0d9bba
AvaPodrazhansky <43285059+AvaPodrazhansky@users.noreply.github.com>
2019-03-23 14:48:04 -0400

    Fixed OS::Load

--- Ravos3/OS.h
+++ Ravos3/OS.h
@@ -18,7 +18,7 @@ public:
 
 	// Loads the boot file onto the disk of the computer.
 	// It is located in the file OS_Loader (so a different people can work on it)	
-	bool OS::Load(std::string filename);
+	bool Load(std::string filename);
 
 	// Test loader - until actual loader is done.  Does not work yet
 	// It is located in the 
