76674dc5ca969f2af95a993d1e474868c663253f
riroepke <rebekah.roepke@icloud.com>
2019-03-23 21:19:38 -0400

    moved load and other loader methods to OS.cpp

--- Ravos3/OS.h
+++ Ravos3/OS.h
@@ -1,10 +1,10 @@
 #pragma once
-#include "OS_Loader.cpp"
+#include "pch.h"
 class OS
 {
 public:
-	Computer *m_Computer;
-	OS(Computer *theComputer);
+	Computer *m_Computer = new Computer();
+	//OS();
 
 
 	std::map<int, PCB*> m_PCB; //map is the C++ version of dictionary
@@ -15,7 +15,7 @@ public:
 	//    (1) Uses Loader routines to read the boot file (containing the jobs) onto the disk
 	//    (2) Creates a PCB for each job
 	// Important: We are creating and filling the disk because our storage is does not survive reboots.
-	bool Boot(const char *Filename);
+	bool Boot(std::string filename);
 
 	//int HexNumToInt(std::string hexstr);
 	//int assignPCB(PCB *tpcb, std::string info);
@@ -23,10 +23,12 @@ public:
 
 	// Loads the boot file onto the disk of the computer.
 	// It is located in the file OS_Loader (so a different people can work on it)	
-	bool Load(std::string filename, Computer *m_Comp);
+	//bool Load(std::string filename, Computer *m_Comp);
+	bool Load(std::string filename);
 
-
-
+	int HexNumToInt(std::string hexstr);
+	int assignPCB(PCB *tpcb, std::string info);
+	int assignDataBuffToPCB(PCB* tpcb, std::string info, int startIndex);
 
 	// Test loader - until actual loader is done.  Does not work yet
 	// It is located in the 
